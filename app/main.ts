83bd8a8300905efd79a0f5aeb7738e1335d09cd2
Adams Banjo <sajioloye@gmail.com>
2024-12-01 16:20:04 -0500

    decode integers

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,8 @@
 // Examples:
 // - decodeBencode("5:hello") -> "hello"
 // - decodeBencode("10:hello12345") -> "hello12345"
-function decodeBencode(bencodedValue: string): string {
+// = decodeBencode("i52e") -> "52"
+function decodeBencode(bencodedValue: string): string | number {
   /* This function is used to decode a bencoded string
     The bencoded string is a string that is prefixed by the length of the string
     **/
@@ -14,7 +15,17 @@ function decodeBencode(bencodedValue: string): string {
     }
     return bencodedValue.substring(firstColonIndex + 1);
   } else {
-    throw new Error("Only strings are supported at the moment");
+    const first = bencodedValue[0];
+    const last = bencodedValue[bencodedValue.length - 1];
+    if (first !== "i" && last !== "e") {
+      throw new Error("Invalid encoded value");
+    }
+    const digit = bencodedValue.slice(1, bencodedValue.length - 1);
+    const parsedDigit = parseInt(digit);
+    if (typeof parsedDigit !== "number") {
+      throw new Error("Invalid encoded value");
+    }
+    return parsedDigit;
   }
 }
 
@@ -25,7 +36,6 @@ if (args[2] === "decode") {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   console.error("Logs from your program will appear here!");
 
-  // Uncomment this block to pass the first stage
   try {
     const decoded = decodeBencode(bencodedValue);
     console.log(JSON.stringify(decoded));
